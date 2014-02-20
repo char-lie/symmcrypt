ee67390e891181f063cd53e7ae1ded1d708b2c57
char-lie <5962274+char-lie@users.noreply.github.com>
2014-02-21 01:06:01 +0200

    Added one more lambda

--- lab1/calculate_index.hs
+++ lab1/calculate_index.hs
@@ -4,7 +4,6 @@ import WordsStatistics
 nn_1 n = n * (n-1)
 
 main = do
-    line <- getLine
-    putStr $ show $
+    getLine >>= (\line -> putStr $ show $
         foldl (\x y -> x + (nn_1 $ snd y)) 0 (charactersFrequencies line) /
-        (nn_1 $ fromIntegral $ length line)
+        (nn_1 $ fromIntegral $ length line))
