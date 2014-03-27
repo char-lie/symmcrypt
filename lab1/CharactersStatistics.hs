81c4131a3712ca72c2749722c07a47ad9b00c87b
char-lie <5962274+char-lie@users.noreply.github.com>
2014-03-27 19:00:50 +0200

    Added bigrams statistics functions

--- lab1/CharactersStatistics.hs
+++ lab1/CharactersStatistics.hs
@@ -3,8 +3,31 @@ import Data.Map
 import Data.Ord
 import Data.List
 
-charactersFrequencies str = (toList $ fromListWith (+) [(c, 1) | c <- str])
-sortedCharactersFrequencies str =
-    sortBy (comparing cmpDesc) (charactersFrequencies str)
-    where
-        cmpDesc tuple = -(snd tuple)
+listToPairs (a:b:xs) = [[a,b]] ++ (listToPairs xs)
+listToPairs [_] = []
+listToPairs [] = []
+
+listToChainedPairs (a:b:xs) = [[a,b]] ++ (listToChainedPairs ([b]++xs))
+listToChainedPairs [_] = []
+listToChainedPairs [] = []
+
+couplesToFrequencies couples = toList $ fromListWith (+) couples
+
+elementsFrequencies lst = couplesToFrequencies [(e, 1) | e <- lst]
+
+charactersFrequencies str = elementsFrequencies str
+
+
+bigramsFrequencies str = elementsFrequencies $ listToPairs str
+
+chainedBigramsFrequencies str = elementsFrequencies $ listToChainedPairs str
+
+sortedFrequencies lst = sortBy (comparing cmpDesc) lst
+    where cmpDesc couple = -(snd couple)
+
+sortedCharactersFrequencies str = sortedFrequencies$charactersFrequencies str
+
+sortedBigramsFrequencies str = sortedFrequencies$bigramsFrequencies str
+
+sortedChainedBigramsFrequencies str =
+    sortedFrequencies$chainedBigramsFrequencies str
--- lab1/DecryptCaesar.hs
+++ lab1/DecryptCaesar.hs
@@ -6,13 +6,14 @@ firstLetter = 'а'
 mostFrequentLetter = 'о'
 lettersInAlphabet = 32
 main =
-    getLine >>= \line -> mapM_ (decypher$mostFrequentCharacter line)
-        ([chr$caesar firstLetter
-            (-2*((ord mostFrequentLetter-(ord$mostFrequentCharacter line))))]
-         ++ line)
+    getLine >>= \line -> mapM_
+        (decypher$mostFrequentCharacter line)
+        ([caesar firstLetter (-2*(mflo-(mfco line)))] ++ line)
     where
         mostFrequentCharacter l = fst ((sortedCharactersFrequencies l)!!0) 
-        decypher base = \c -> putStr[chr$caesar c
-            (ord mostFrequentLetter-(ord base))]
-        caesar c shift = (ord c - ord firstLetter + shift)
+        mflo = ord mostFrequentLetter
+        mfco = ord.mostFrequentCharacter
+        getShift base = (mflo-(ord base))
+        decypher base = \c -> putStr [caesar c (getShift base)]
+        caesar c shift = chr$(ord c - ord firstLetter + shift)
             `mod` lettersInAlphabet + (ord firstLetter)
